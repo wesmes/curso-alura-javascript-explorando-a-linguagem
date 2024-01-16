e0b9b8c5d3e1fee9a8ad8c5e227446eeecdeb3aa
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-01-16 06:25:01 -0300

    Módulo 2 - Aula 5: Atribuição e imutabilidade

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuição de variáveis.");
+
+const idade = 29;
+let nome = "Ricardo";
+const sobrenome = "Bugan";
+const ano = 2024;
+
+// console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+nomeCompleto = 2;
