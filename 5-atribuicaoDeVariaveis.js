101cb0cb35d9ee230facd2c2b0396c1630c6e345
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-01-16 06:42:58 -0300

    Módulo 3 - Aula 2: Criando listas

--- 5-atribuicaoDeVariaveis.js
+++ 5-atribuicaoDeVariaveis.js
@@ -1,6 +1,6 @@
 console.log("Trabalhando com atribuição de variáveis.");
 
-const idade = 29;
+// const idade = 29;
 let nome = "Ricardo";
 const sobrenome = "Bugan";
 const ano = 2024;
@@ -12,4 +12,11 @@ console.log(`Meu nome é ${nome} ${sobrenome}`);
 const nomeCompleto = nome + sobrenome;
 console.log(nomeCompleto);
 
-nomeCompleto = 2;
+// nomeCompleto = 2;
+
+let idade; // declarando variável
+idade = 26; // atribuindo valor
+idade = idade+1;
+console.log(idade);
+
+
