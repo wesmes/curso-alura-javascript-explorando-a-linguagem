c1e202fc94b60d57283b06a44bd2439e101c2eb9
Weslley Gomes da Cruz <w_gomes@protonmail.com>
2024-01-17 05:18:11 -0300

    Módulo 5 - Aula 4: Break e Debugger

--- 8-loops.js
+++ 8-loops.js
@@ -17,15 +17,19 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
+let destinoExiste = false;
+
 while (contador < 3) {
-    console.log(listaDeDestinos[contador]);
+    
     if (listaDeDestinos[contador] == destino) {
         console.log("Destino existe");
-    } else {
-        console.log("Destino não existe");
-    }
+        destinoExiste = true;
+        break;
+    } 
     contador++;
 }
+
+console.log("Destino existe: ", destinoExiste);
 
 // if (podeComprar) {
 //     console.log("Boa viagem!");
